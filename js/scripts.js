11b2355587bdd12f9b4ec50ca2a158483c0b607f
Alex Williams and Daniel Lira <alex@>
2018-10-25 16:23:51 -0700

    Created a webpage that takes user input and encrypts it.

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+//Business Logic
+counter = 0;
+temp = 0;
+rotation = 0;
+var cryptoArray = [];
+var outputCryptoArray = [];
+var finalOutputArray = [];
+var cryptoCalc = function(userString) {
+  //debugger;
+  var userStringSplit = userString.split(" ");
+  var userStringFiltered = userStringSplit.filter(userStringFiltered => userStringFiltered.length >= 1);
+  var userStringJoined = userStringFiltered.join("");
+  var userStringLength = userStringJoined.length;
+  var squareRoot = Math.floor(Math.sqrt(userStringLength));
+  //debugger;
+  for (x = 0;x <= squareRoot; x++) {
+    this["cryptoArray"+x] = userStringJoined.slice(0,squareRoot);
+    userStringJoined = userStringJoined.slice(squareRoot);
+  };
+  while(counter <= squareRoot) {
+    for (z = 0;z <= squareRoot; z++) {
+      var tempCrypto = this["cryptoArray"+z];
+      outputCryptoArray.push(tempCrypto[counter]);
+    };
+    counter++;
+  };
+  outputCryptoArray = outputCryptoArray.join('').toLowerCase();
+
+  for (p=0;p <= (userStringLength/5);p++) {
+    tempSplitArray = outputCryptoArray.slice(0,5);
+    outputCryptoArray = outputCryptoArray.slice(5);
+    finalOutputArray.push(tempSplitArray);
+    finalOutputArray.push(" ");
+  };
+  finalOutputArray = finalOutputArray.join('');
+  return finalOutputArray;
+
+}
+
+
+
+//User Interface Logic
+$(document).ready(function() {
+  $("form#formInput").submit(function(event) {
+    event.preventDefault();
+    var userString = $("textarea#userInput").val();
+    cryptoCalc(userString);
+    $("#userOutput").text(finalOutputArray);
+  });
+});
